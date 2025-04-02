a44c7659e542cc2247b109592553fd19c2086863
Jon Aksel <jonakselm@gmail.com>
2025-04-02 15:07:31 +0200

    Updated gitignore

--- build/qube_bringup/prefix_override/sitecustomize.py
+++ build/qube_bringup/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/aron-larsen/miniProject_ws/install/qube_bringup'
+    sys.prefix = sys.exec_prefix = '/home/user/miniProject/install/qube_bringup'
