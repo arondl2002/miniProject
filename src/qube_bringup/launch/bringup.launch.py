495a1e06f5517d59b20153c688fe8f7a0e1ad046
arondl2002 <143505695+arondl2002@users.noreply.github.com>
2025-04-22 18:39:02 +0200

    Update bringup.launch.py
    La til kommentarer der det virket passende

--- src/qube_bringup/launch/bringup.launch.py
+++ src/qube_bringup/launch/bringup.launch.py
@@ -1,3 +1,4 @@
+# Import av nødvendige bibliotek
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
@@ -8,12 +9,14 @@ from launch_ros.actions import Node
 import xacro
 
 def generate_launch_description():
+    # Inkluderer ekstern launch-script for å initialisere 
     qube_driver_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(get_package_share_directory("qube_driver"), "launch", "qube_driver.launch.py")
         )
     )
 
+    # Konfigurasjonsfilen for RViz
     rviz_config = os.path.join(get_package_share_directory("qube_description"), "rviz", "config.rviz")
     rviz_node = Node(
         package = "rviz2",
@@ -23,22 +26,26 @@ def generate_launch_description():
         output = "screen"
     )
 
+    # Initialiserer RViz med angitte konfigurasjonsfiler
     urdf_file = os.path.join(get_package_share_directory("qube_bringup"), "urdf", "controlled_qube.urdf.xacro")
     robot_description = xacro.process_file(urdf_file).toprettyxml()
     params = {"robot_description": robot_description}
 
+    # Starter publisher for robottilstand for å publisere leddtransformasjonene til Kuben (Qube)
     robot_state_publisher_node = Node(
         package = "robot_state_publisher",
         executable = "robot_state_publisher",
         parameters = [params]
     )
 
+    # Initialiserer publisher for ledd tilstand for å publisere felles leddinformasjon
     joint_state_publisher_node = Node(
         package="joint_state_publisher",
         executable="joint_state_publisher",
         name="joint_state_publisher",
     )
 
+    # Liste over alle noder og launch-variabler (elementer) som skal initialiseres
     nodes_to_start = [
         robot_state_publisher_node,
         joint_state_publisher_node,
@@ -46,6 +53,7 @@ def generate_launch_description():
         qube_driver_launch
     ]
 
+    # Returnere hele Launch beskrivelsen med alle elemeter, samt et argument for robot beskrivelsen
     return LaunchDescription([
         DeclareLaunchArgument(
             "robot_description",
